672cee9e58c2b85c659636774f02dc5f32f5097d
michal kolarzyk <michalkolarzyk@gmail.com>
2021-11-14 16:05:55 +0100

    files cache

--- RestaurantAPI/Controllers/FileControler.cs
+++ RestaurantAPI/Controllers/FileControler.cs
@@ -15,6 +15,7 @@ namespace RestaurantAPI.Controllers
     {
         [Authorize]
         [HttpGet]
+        [ResponseCache(Duration = 1200, VaryByQueryKeys = new[] { "fileName" })]
         public ActionResult GetFile([FromQuery] string fileName)
         {
             string rootPath = Directory.GetCurrentDirectory();
